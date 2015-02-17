487214e8daf36995e5b3a0e44ffa7a7330d91867
andy <andrzejsz@gmail.com>
2015-02-17 15:21:40 +0000

    scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217143239) do
+ActiveRecord::Schema.define(version: 20150217150248) do
 
   create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "published_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
